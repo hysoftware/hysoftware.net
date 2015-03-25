4cd2236f644dcdd735d51934c75512ed6b5ce379
Hiroaki Yamamoto <admin@hysoftware.net>
2015-03-25 19:37:51 +0900

    Use Venv on "the top"

--- grunt/shell.js
+++ grunt/shell.js
@@ -1,5 +1,5 @@
-/*global exports, require, process*/
-(function (e, r, venv) {
+/*global exports, require*/
+(function (e, r) {
     "use strict";
     var common = r("./common.js"),
         pathJoin = r("path").join,
@@ -7,14 +7,10 @@
             "manage.py"
         ].concat(common.hysoft_modules).join(" "),
         backend_test_commands = [
-            "echo flake8",
             "flake8 -j auto --exclude=" + common.exclude.backend.join(",") +
                 " " + files,
-            "echo pylint",
             "pylint --disable=locally-disabled,locally-enabled --ignore=migrations " + files,
-            "echo python manage.py migrate",
             "python manage.py migrate",
-            "echo python manage.py loaddata",
             "python manage.py loaddata " + [
                 "about/fixtures/hiroaki.json"
             ].join(" "),
@@ -31,10 +27,8 @@
         },
         "backend-syntax-check-ci": {
             "command": [].concat(
-                "source " + pathJoin(venv, "bin/activate"),
-                backend_test_commands,
-                "deactivate"
+                backend_test_commands
             ).join("&&")
         }
     };
-}(exports, require, process.env.SHIPPABLE_VE_DIR));
+}(exports, require));
