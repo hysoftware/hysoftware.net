044771316d7249d28eb32b015c9bb43aead58082
Hiroaki Yamamoto <admin@hysoftware.net>
2015-03-25 18:30:52 +0900

    Added echo for each build step

--- grunt/shell.js
+++ grunt/shell.js
@@ -5,26 +5,31 @@
         files = [
             "manage.py"
         ].concat(common.hysoft_modules).join(" "),
-        backend_syntax_commands = [
+        backend_test_commands = [
+            "echo flake8",
             "flake8 -j auto --exclude=" + common.exclude.backend.join(",") +
                 " " + files,
+            "echo pylint",
             "pylint --disable=locally-disabled,locally-enabled --ignore=migrations " + files,
+            "echo python manage.py migrate",
             "python manage.py migrate",
+            "echo python manage.py loaddata",
             "python manage.py loaddata " + [
                 "about/fixtures/hiroaki.json"
             ].join(" "),
+            "echo python manage.py test",
             "python manage.py test"
         ];
     e.shell = {
         "backend-syntax-check-dev": {
             "command": [].concat(
                 "source ../bin/activate",
-                backend_syntax_commands,
+                backend_test_commands,
                 "deactivate"
             ).join("&&")
         },
         "backend-syntax-check-ci": {
-            "command": backend_syntax_commands.join("&&")
+            "command": backend_test_commands.join("&&")
         }
     };
 }(exports, require));
