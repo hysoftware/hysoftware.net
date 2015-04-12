ca86da53da0f5aec0167ec3213684fc7cbe9e6ee
Hiroaki Yamamoto <admin@hysoftware.net>
2015-04-12 19:51:01 +0900

    Removed ping function because there was no effects

--- hysoft/wsgi.py
+++ hysoft/wsgi.py
@@ -13,23 +13,7 @@ import os
 import requests
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-from uwsgidecorators import timer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hysoft.settings")
 # pylint: disable=invalid-name
 application = DjangoWhiteNoise(get_wsgi_application())
-
-
-# 1800 secs = 30 minutes
-@timer(1800)
-def ping(pingnum):
-    '''
-    According to heroku document (See below), single dyno sleeps when
-    there is no action in 1 hour. This ping function prevents this sleep:
-    https://devcenter.heroku.com/articles/dynos#dyno-sleeping
-    '''
-    # pylint: disable=unused-argument
-    destination = os.environ.get("PING_DEST", "http://localhost:50000/")
-    requests.get(destination)
-
-# pylint: enable=unused-argument
