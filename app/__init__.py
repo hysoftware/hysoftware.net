7d77b6a62afd7505d31858f9608f07f5ed4dc52f
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-02-20 18:36:29 +0900

    Changed app config load process

--- app/__init__.py
+++ app/__init__.py
@@ -2,26 +2,52 @@
 # coding=utf-8
 
 import os
+import htmlmin
 
 from flask import Flask
-from flask.ext.wtf import CsrfProtect
+from flask.ext.wtf.csrf import CsrfProtect, generate_csrf
 from flask.ext.debugtoolbar import DebugToolbarExtension
 
 from .home import home
 
 cfgmap = {
-    "production": "app..config.ProductionConfig",
+    "production": "app.config.ProductionConfig",
     "devel": "app.config.DevelConfig"
 }
 
 app = Flask(__name__)
 app.config.from_object(
-    cfgmap.get(os.environ.get("mode", "devel"), "app.config.DevelConfig")
+    cfgmap[os.environ.get("mode", "devel")]
 )
 DebugToolbarExtension(app)
 CsrfProtect(app)
 
 app.register_blueprint(home)
+
+
+@app.after_request
+def html_minification(resp):
+    '''
+    Minify HTML when response mimetype is text/html
+    '''
+    if resp.mimetype == "text/html":
+        resp.data = htmlmin.minify(
+            resp.data.decode("utf-8"),
+            remove_comments=True,
+            remove_empty_space=True,
+            remove_optional_attribute_quotes=False
+        )
+    return resp
+
+
+@app.after_request
+def csrf_prevent(resp):
+    '''
+    Add CSRF Preventation for angularJS
+    '''
+
+    resp.set_cookie("X-CSRFToken", generate_csrf())
+    return resp
 
 
 __all__ = ["app"]
--- app/config/__init__.py
+++ app/config/__init__.py
@@ -9,12 +9,11 @@ import os
 
 mode = os.environ.get("mode", "devel")
 
-
-if mode == "devel":
-    from .devel import DevelConfig
-
 if mode == "production":
     from .production import ProductionConfig
-
+elif mode == "devel":
+    from .devel import DevelConfig
+else:
+    EnvironmentError("Invalid mode is given")
 
 __all__ = ["ProductionConfig", "DevelConfig"]
