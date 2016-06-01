b55a305f25e18ed0b4217f50f7223150be809a75
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-06-01 16:25:59 +0900

    Cover some tests

---
+++ app/home/controllers/home.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""Home view."""
+
+from random import choice
+
+from flask import render_template, session
+from flask.ext.classy import FlaskView
+from flask.ext.login import current_user
+
+from .index import IndexView
+from ..forms import ContactForm
+
+
+class HomeView(FlaskView):
+    """Home view."""
+
+    trailing_slash = False
+
+    def index(self):
+        """Index reosurce."""
+        if "tagline" not in session:
+            session["tagline"] = choice(IndexView.website_taglines)
+        return render_template(
+            "home.html", tagline=session["tagline"],
+            current_user=current_user, contact_form=ContactForm()
+        )
---
+++ app/home/forms/contact.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""Contact form."""
+
+from flask.ext.wtf import Form, RecaptchaField
+import wtforms.fields as fld
+import wtforms.validators as vld
+
+
+class ContactForm(Form):
+    """Contact Form."""
+
+    name = fld.StringField(
+        "Your Name", validators=[vld.InputRequired()],
+        render_kw={
+            "class": "form-control",
+            "data-ng-model": "contact.name",
+            "data-ng-disabled": "contactForm.$submitted",
+            "required": True
+        }
+    )
+    company_name = fld.StringField(
+        "Your Company Name", validators=[vld.Optional()],
+        render_kw={
+            "class": "form-control",
+            "data-ng-model": "contact.company",
+            "data-ng-disabled": "contactForm.$submitted"
+        }
+    )
+    email = fld.StringField(
+        "Email", validators=[vld.Email(), vld.InputRequired()],
+        render_kw={
+            "class": "form-control",
+            "data-ng-model": "contact.email",
+            "data-ng-disabled": "contactForm.$submitted",
+            "required": True
+        }
+    )
+    message = fld.TextAreaField(
+        "Message", validators=[vld.InputRequired()],
+        render_kw={
+            "class": "form-control",
+            "data-ng-model": "contact.message",
+            "data-ng-disabled": "contactForm.$submitted",
+            "required": True
+        }
+    )
+    recaptcha = RecaptchaField(
+        "I am not a bot", validators=[vld.InputRequired()],
+        render_kw={
+            "data-ng-model": "contact.recaptcha",
+            "data-ng-disabled": "contactForm.$submitted",
+            "required": True
+        }
+    )
