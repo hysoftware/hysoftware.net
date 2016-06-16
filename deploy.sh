5486217714411ce662604a8a660254ea82e09e6c
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-06-16 16:26:43 +0900

    Added debug

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,30 @@
 # fi
 
 pushd ${HOME}
+echo "Cloning deploy repo"
 git clone ${DEPLOY_REPO} deploy > /dev/null
+
+echo "Moving git control"
 mv deploy/.git git
 
+echo "Copying files to the repo"
 rsync --delete \
   --delete-excluded \
   --exclude-from=${TRAVIS_BUILD_DIR}/exludelist.txt \
   -aP ${TRAVIS_BUILD_DIR}/ ${HOME}/deploy
 
+echo "Putting git control back"
 mv git deploy/.git
+
+echo "cd the repo"
 cd depoloy
+
+echo "git add ."
 git add . > /dev/null
 # git commit -m "Release for ${TRAVIS_TAG}" > /dev/null
+echo "git commit"
 git commit -m "Release for $(date +%s)" > /dev/null
+
+echo "Pushing"
 git push origin master > /dev/null
 popd
