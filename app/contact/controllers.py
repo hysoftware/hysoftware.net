8548017c403bea70600864116fe1556c437d9fd6
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-06-23 18:46:25 +0900

    The mail to potential cliend should be sent first

--- app/contact/controllers.py
+++ app/contact/controllers.py
@@ -39,39 +39,37 @@ class ContactView(FlaskView):
         if not form.validate():
             return make_response(jsonify(form.errors), 417)
         person = Person.objects(id=ObjectId(form.to.data)).get()
-        member_resp = requests.post(
-            urljoin(current_app.config["MAILGUN_URL"] + "/", "messages"),
-            auth=("api", current_app.config["MAILGUN_API"]),
-            data={
-                "from": form.email.data,
-                "to": person.email,
-                "subject": "Someone wants to contact you (hysoftware.net)",
-                "text": render_template(
-                    "mail_to_member.txt", form=form, member=person
-                ),
-                "html": render_template(
-                    "mail_to_member.html", form=form, member=person
-                )
-            }
-        )
-        cli_resp = requests.post(
-            urljoin(current_app.config["MAILGUN_URL"] + "/", "messages"),
-            auth=("api", current_app.config["MAILGUN_API"]),
-            data={
-                "from": "HYSOFT Mailbot <noreply@hysoftware.net>",
-                "to": form.email.data,
-                "subject": "Thanks for your interest!",
-                "text": render_template(
-                    "mail_to_client.txt", form=form, member=person
-                ),
-                "html": render_template(
-                    "mail_to_client.html", form=form, member=person
-                )
-            }
-        )
         try:
-            member_resp.raise_for_status()
-            cli_resp.raise_for_status()
+            requests.post(
+                urljoin(current_app.config["MAILGUN_URL"] + "/", "messages"),
+                auth=("api", current_app.config["MAILGUN_API"]),
+                data={
+                    "from": "HYSOFT Mailbot <noreply@hysoftware.net>",
+                    "to": form.email.data,
+                    "subject": "Thanks for your interest!",
+                    "text": render_template(
+                        "mail_to_client.txt", form=form, member=person
+                    ),
+                    "html": render_template(
+                        "mail_to_client.html", form=form, member=person
+                    )
+                }
+            ).raise_for_status()
+            requests.post(
+                urljoin(current_app.config["MAILGUN_URL"] + "/", "messages"),
+                auth=("api", current_app.config["MAILGUN_API"]),
+                data={
+                    "from": form.email.data,
+                    "to": person.email,
+                    "subject": "Someone wants to contact you (hysoftware.net)",
+                    "text": render_template(
+                        "mail_to_member.txt", form=form, member=person
+                    ),
+                    "html": render_template(
+                        "mail_to_member.html", form=form, member=person
+                    )
+                }
+            ).raise_for_status()
         except requests.HTTPError as e:
             return make_response(
                 jsonify({"Mail": [e.response.json().get("message")]}),
