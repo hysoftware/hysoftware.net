9660464051cfb2d40af00c46039cb6bcb2d9c5a9
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-12-03 23:14:17 +0900

    Updated the dependency

--- app/user/forms.py
+++ app/user/forms.py
@@ -24,7 +24,7 @@ class ContactForm(AngularForm, forms.ModelForm):
         no_label = ("nobot", )
 
         widgets = {
-            "user": MDSelect()
+            "user": MDSelect(disable_select=True)
         }
 
     nobot = ReCaptchaField()
@@ -33,5 +33,4 @@ class ContactForm(AngularForm, forms.ModelForm):
         """Init the instance."""
         info_id = kwargs.pop("info_id", None)
         super(ContactForm, self).__init__(*args, **kwargs)
-        print(self.fields["user"].initial)
         self.fields["user"].initial = info_id
