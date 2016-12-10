26caaa27e8d913861fed22a998af86fbb425f491
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-12-10 13:01:16 +0900

    Changed authentication algorithm

--- app/settings/devel.py
+++ app/settings/devel.py
@@ -91,6 +91,12 @@ class DevelConfig(DjangoDefaults):
         ]
     )
 
+    PASSWORD_HASHERS = (
+        "django.contrib.auth.hashers.Argon2PasswordHasher",
+        "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+        "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher"
+    )
+
     LANGUAGE_CODE = 'en-us'
     TIME_ZONE = 'UTC'
     USE_I18N = True
