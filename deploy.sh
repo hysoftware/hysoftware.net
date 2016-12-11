2495754aab6ff0c9620cd463d564e1c3de583660
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-12-12 01:10:11 +0900

    Needs to be shown ...still

--- deploy.sh
+++ deploy.sh
@@ -8,20 +8,20 @@ if [ -z ${TRAVIS_TAG} ]; then
 fi
 
 cd ${HOME}
-git clone ${DEPLOY_REPO} deploy #> /dev/null 2>&1
+git clone ${DEPLOY_REPO} deploy > /dev/null 2>&1
 
 mv deploy/.git git
 rsync --delete \
   --delete-excluded \
   --exclude-from=${TRAVIS_BUILD_DIR}/excludelist.txt \
-  -aP ${TRAVIS_BUILD_DIR}/ ${HOME}/deploy #> /dev/null 2>&1
+  -aP ${TRAVIS_BUILD_DIR}/ ${HOME}/deploy > /dev/null 2>&1
 mv git deploy/.git
 
 cd deploy
 git add --all . > /dev/null 2>&1
-git config --global user.email "build@travis" #> /dev/null 2>&1
-git config --global user.name "Travis CI" #> /dev/null 2>&1
-git commit -m "Release for ${TRAVIS_TAG}" #> /dev/null 2>&1
-git tag -a ${TRAVIS_TAG} -m ${TRAVIS_TAG} #> /dev/null 2>&1
-git push origin master ${TRAVIS_TAG} #> /dev/null 2>&1
+git config --global user.email "build@travis" > /dev/null 2>&1
+git config --global user.name "Travis CI" > /dev/null 2>&1
+git commit -m "Release for ${TRAVIS_TAG}" > /dev/null 2>&1
+git tag -a ${TRAVIS_TAG} -m ${TRAVIS_TAG} > /dev/null 2>&1
+git push origin master ${TRAVIS_TAG} > /dev/null 2>&1
 cd ${TRAVIS_BUILD_DIR}
