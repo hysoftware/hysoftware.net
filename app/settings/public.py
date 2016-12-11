14a813fa395af18e1c66ec37296224bc7b8151d7
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-12-11 11:17:29 +0900

    Added uwsgi config

--- app/settings/public.py
+++ app/settings/public.py
@@ -3,6 +3,7 @@
 
 """Config for public."""
 
+import re
 import os
 
 from .devel import DevelConfig
@@ -12,7 +13,19 @@ class PublicConfig(DevelConfig):
     """Config for production."""
 
     SECRET_KEY = os.environ["SECRET"]
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ["DB_ENGINE"],
+            "NAME": os.environ["DB_NAME"],
+            "USER": os.environ["DB_USER"],
+            "PASSWORD": os.environ["DB_PW"],
+            "HOST": os.environ["DB_HOST"],
+            "PORT": os.environ["DB_PORT"]
+        }
+    }
     RECAPTCHA_PUBLIC_KEY = os.environ["RECAPTCHA_PUBLIC_KEY"]
     RECAPTCHA_PRIVATE_KEY = os.environ["RECAPTCHA_PRIVATE_KEY"]
     MAILGUN_KEY = os.environ["MAILGUN_KEY"]
     MAILGUN_URL = os.environ["MAILGUN_URL"]
+    ALLOWED_HOSTS = re.split(",\\s.", os.environ["ALLOWED_HOSTS"])
+    CELERY_BROKER_URL = os.environ["CELERY_BROKER_URL"]
--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,7 @@
 
 """Setting test."""
 
+import re
 from unittest.mock import patch
 
 from django.test import TestCase
@@ -18,8 +19,19 @@ class ProductionConfigTest(TestCase):
             "RECAPTCHA_PUBLIC_KEY": "recaptcha_test_pubkey",
             "RECAPTCHA_PRIVATE_KEY": "recaptcha_test_privkey",
             "MAILGUN_KEY": "mailgun-key",
-            "MAILGUN_URL": "mailgun-url"
+            "MAILGUN_URL": "mailgun-url",
+            "ALLOWED_HOSTS": (
+                "allowed.io, allowed2.io,allowed3.io, allowed4.io,"
+            ),
+            "CELERY_BROKER_URL": "test_broker",
+            "DB_ENGINE": "test_db_engine",
+            "DB_NAME": "test_db_name",
+            "DB_USER": "test_db_user",
+            "DB_PW": "test_db_pw",
+            "DB_HOST": "test_db_host",
+            "DB_PORT": "test_db_port"
         }
+        self.sep = re.compile(",\\s.")
         with patch.dict("os.environ", self.environ):
             from app.settings.public import PublicConfig
             self.conf_p = PublicConfig
@@ -60,3 +72,44 @@ class ProductionConfigTest(TestCase):
         self.assertNotEqual(self.conf_p.MAILGUN_URL, self.conf_d.MAILGUN_URL)
         self.assertEqual(self.conf_p.MAILGUN_KEY, self.environ["MAILGUN_KEY"])
         self.assertEqual(self.conf_p.MAILGUN_URL, self.environ["MAILGUN_URL"])
+
+    def test_allowed_hosts(self):
+        """Allowed host test."""
+        self.assertNotEqual(
+            self.conf_p.ALLOWED_HOSTS,
+            self.conf_d.ALLOWED_HOSTS
+        )
+        self.assertEqual(
+            self.conf_p.ALLOWED_HOSTS,
+            self.sep.split(self.environ["ALLOWED_HOSTS"])
+        )
+
+    def test_celery_broker(self):
+        """Celery broker should be differ from devel env."""
+        self.assertNotEqual(
+            self.conf_p.CELERY_BROKER_URL,
+            self.conf_d.CELERY_BROKER_URL
+        )
+        self.assertEqual(
+            self.conf_p.CELERY_BROKER_URL,
+            self.environ["CELERY_BROKER_URL"]
+        )
+
+    def test_db(self):
+        """Database URL should be differ from devel."""
+        # "DB_ENGINE": "test_db_engine",
+        # "DB_NAME": "test_db_name",
+        # "DB_PW": "test_db_pw",
+        # "DB_HOST": "test_db_host",
+        # "DB_PORT": "test_db_port"
+        self.assertNotEqual(self.conf_p.DATABASES, self.conf_d.DATABASES)
+        self.assertDictEqual({
+            "default": {
+                "ENGINE": self.environ["DB_ENGINE"],
+                "NAME": self.environ["DB_NAME"],
+                "USER": self.environ["DB_USER"],
+                "PASSWORD": self.environ["DB_PW"],
+                "HOST": self.environ["DB_HOST"],
+                "PORT": self.environ["DB_PORT"]
+            }
+        }, self.conf_p.DATABASES)
