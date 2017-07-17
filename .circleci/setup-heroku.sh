a01158eb3ace07d75758ce73d273d597a03d201b
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2017-07-17 16:20:54 +0900

    Move fingerprint to steps level

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-    wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+  wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
   mkdir -p /usr/local/lib /usr/local/bin
   tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
   ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
