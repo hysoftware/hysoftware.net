3e909b1773f43c9e201f73275b1837048ca460c8
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-06-12 21:03:07 +0900

    [WIP]: Making ABOUT page db-less

--- app/user/views.py
+++ app/user/views.py
@@ -5,6 +5,7 @@
 
 import json
 
+from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
@@ -13,12 +14,15 @@ from django.utils.translation import (
     ungettext as _n, ugettext_lazy as _lz
 )
 
+from hysoftware_data.users import Users
+
 
 class AboutView(TemplateView):
     """About page."""
 
     template_name = "about.html"
     ng_app = "user"
+    users = Users(settings.NAME)
 
     @cached_property
     def description(self):
@@ -28,20 +32,19 @@ class AboutView(TemplateView):
     @cached_property
     def users_info(self):
         """Return users information."""
-        from .models import UserInfo
-        return UserInfo.objects
+        return self.users
 
 
 class MemberDialog(TemplateView):
     """Staff Dialog."""
 
     template_name = "member_dialog.html"
+    users = Users(settings.NAME)
 
     @cached_property
     def user_info(self):
         """Return the user information."""
-        from .models import UserInfo
-        return get_object_or_404(UserInfo, id=self.kwargs["info_id"])
+        return get_object_or_404(self.users, id=self.kwargs["info_id"])
 
 
 class ContactView(TemplateView):
@@ -50,18 +53,17 @@ class ContactView(TemplateView):
     template_name = "contact.html"
     ng_app = "user"
     description = _lz("Contact Form")
+    users = Users(settings.NAME)
 
     @cached_property
     def user_info(self):
         """Return the user information."""
-        from .models import UserInfo
-        return get_object_or_404(UserInfo, id=self.kwargs["info_id"])
+        return get_object_or_404(self.users, id=self.kwargs["info_id"])
 
     @cached_property
     def users_info(self):
         """Return UserInfo.objects."""
-        from .models import UserInfo
-        return UserInfo.objects
+        return self.users
 
     @cached_property
     def form(self):
