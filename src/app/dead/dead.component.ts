7147a13e5941eed6d458e4ab9106ca5dcfff3ddd
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-01-04 09:13:13 +0900

    Fixed lint

--- src/app/dead/dead.component.ts
+++ src/app/dead/dead.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dead',
   styleUrls: ['./dead.component.scss'],
   templateUrl: './dead.component.html',
 })
-export class DeadComponent implements OnInit {
+export class DeadComponent {
 
   @Input() public rip: number;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
