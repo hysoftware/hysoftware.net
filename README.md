4e4a8c2190d5983b2bf77f619d9f93cd7375f600
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-01-20 11:15:11 +0900

    docs: 📝 Edit README.md

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-# Development
+# hysoftware.net webapp
+[![Test Badge]][Test Link]
+[![Code Climate]][Code Climate Link]
+[![Test Coverage]][Test Coverage Link]
 
-Your new bare-bones project includes minimal organization with a single `main.rs` file and a few assets.
+[Test Badge]: https://github.com/hysoftware/hysoftware.net/actions/workflows/test.yml/badge.svg
+[Test Link]: https://github.com/hysoftware/hysoftware.net/actions/workflows/test.yml
+[Code Climate]: https://codeclimate.com/github/hysoftware/hysoftware.net/badges/gpa.svg
+[Code Climate Link]: https://codeclimate.com/github/hysoftware/hysoftware.net
+[Test Coverage]: https://api.codeclimate.com/v1/badges/b57a7bcc3b1fafbc8529/test_coverage
+[Test Coverage Link]: https://codeclimate.com/github/hysoftware/hysoftware.net/test_coverage
 
-### Serving Your App
+## What this?
+This is a website of [hysoftware].
 
-Run the following command in the root of your project to start developing with the default platform:
+[hysoftware]: https://www.hysoftware.net/
 
-```bash
-dx serve
-```
+## License
 
-To run for a different platform, use the `--platform platform` flag. E.g.
-```bash
-dx serve --platform desktop
-```
-
+See [LICENSE.md](LICENSE.md).
--- src/main.rs
+++ src/main.rs
@@ -10,94 +10,104 @@ enum Route {
     Blog { id: i32 },
 }
 
-const BOOTSTRAP_CSS: Asset = asset!("/node/node_modules/bootstrap/dist/css/bootstrap.css");
-const BOOTSTRAP_JS: Asset = asset!("/node/node_modules/bootstrap/dist/js/bootstrap.js");
+const BOOTSTRAP_CSS: Asset =
+  asset!("/node/node_modules/bootstrap/dist/css/bootstrap.css");
+const BOOTSTRAP_JS: Asset =
+  asset!("/node/node_modules/bootstrap/dist/js/bootstrap.js");
+
+const FONTAWESOME_CSS: Asset =
+  asset!("/node/node_modules/@fortawesome/fontawesome-svg-core/styles.css");
+const FONTAWESOME_JS: Asset =
+  asset!("/node/node_modules/@fortawesome/fontawesome-svg-core/index.js");
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 fn main() {
-    dioxus::launch(App);
+  dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Router::<Route> {}
-    }
+  rsx! {
+      document::Link { rel: "icon", href: FAVICON }
+      document::Link { rel: "stylesheet", href: BOOTSTRAP_CSS }
+      document::Link { rel: "stylesheet", href: MAIN_CSS }
+      document::Link { rel: "stylesheet", href: FONTAWESOME_CSS }
+      document::Link { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500" }
+      document::Script { src: BOOTSTRAP_JS, defer: true, async: true }
+      document::Script { src: FONTAWESOME_JS, defer: true, async: true }
+      Router::<Route> {}
+  }
 }
 
 #[component]
 pub fn Hero() -> Element {
-    rsx! {
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
-        }
-    }
+  rsx! {
+      div {
+          id: "hero",
+          div { id: "links",
+              a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+              a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+              a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+              a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+              a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+              a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+          }
+      }
+  }
 }
 
 /// Home page
 #[component]
 fn Home() -> Element {
-    rsx! {
-        Hero {}
+  rsx! {
+      Hero {}
 
-    }
+  }
 }
 
 /// Blog page
 #[component]
 pub fn Blog(id: i32) -> Element {
-    rsx! {
-        div {
-            id: "blog",
+  rsx! {
+      div {
+          id: "blog",
 
-            // Content
-            h1 { "This is blog #{id}!" }
-            p { "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components." }
+          // Content
+          h1 { "This is blog #{id}!" }
+          p { "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components." }
 
-            // Navigation links
-            Link {
-                to: Route::Blog { id: id - 1 },
-                "Previous"
-            }
-            span { " <---> " }
-            Link {
-                to: Route::Blog { id: id + 1 },
-                "Next"
-            }
-        }
-    }
+          // Navigation links
+          Link {
+              to: Route::Blog { id: id - 1 },
+              "Previous"
+          }
+          span { " <---> " }
+          Link {
+              to: Route::Blog { id: id + 1 },
+              "Next"
+          }
+      }
+  }
 }
 
 /// Shared navbar component.
 #[component]
 fn Navbar() -> Element {
-    rsx! {
-        div {
-            id: "navbar",
-            Link {
-                to: Route::Home {},
-                "Home"
-            }
-            Link {
-                to: Route::Blog { id: 1 },
-                "Blog"
-            }
-        }
+  rsx! {
+      div {
+          id: "navbar",
+          Link {
+              to: Route::Home {},
+              "Home"
+          }
+          Link {
+              to: Route::Blog { id: 1 },
+              "Blog"
+          }
+      }
 
-        Outlet::<Route> {}
-    }
+      Outlet::<Route> {}
+  }
 }
