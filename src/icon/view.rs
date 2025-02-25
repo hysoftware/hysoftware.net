9d1381915ee7515325807587fe6d071f48e4634a
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-02-26 08:43:06 +0900

    test: ✅ Add tests of icons

--- src/icon/view.rs
+++ src/icon/view.rs
@@ -18,3 +18,54 @@ pub fn Icon(icon: Icons) -> Element {
     }
   };
 }
+
+#[cfg(test)]
+mod tests {
+  use super::{Icon, IconProps};
+  use crate::icon::fontawesome::{GITHUB, GITLAB, KEYBASE};
+  use crate::icon::icons::Icons;
+  use dioxus::prelude::*;
+  use pretty_assertions::assert_eq;
+
+  #[test]
+  fn test_icon_github() {
+    let icon = dioxus_ssr::render_element(Icon(IconProps {
+      icon: Icons::FaGithub,
+    }));
+    let correct = dioxus_ssr::render_element(rsx! {
+      div {
+        class: "icon",
+        dangerous_inner_html: GITHUB,
+      }
+    });
+    assert_eq!(icon, correct);
+  }
+
+  #[test]
+  fn test_icon_gitlab() {
+    let icon = dioxus_ssr::render_element(Icon(IconProps {
+      icon: Icons::FaGitlab,
+    }));
+    let correct = dioxus_ssr::render_element(rsx! {
+      div {
+        class: "icon",
+        dangerous_inner_html: GITLAB,
+      }
+    });
+    assert_eq!(icon, correct);
+  }
+
+  #[test]
+  fn test_icon_keybase() {
+    let icon = dioxus_ssr::render_element(Icon(IconProps {
+      icon: Icons::FaKeybase,
+    }));
+    let correct = dioxus_ssr::render_element(rsx! {
+      div {
+        class: "icon",
+        dangerous_inner_html: KEYBASE,
+      }
+    });
+    assert_eq!(icon, correct);
+  }
+}
