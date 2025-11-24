e963130dfb71f3533bd560973b7ee0ffce4618f5
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-11-24 10:28:48 +0900

    feat: Add content

---
+++ src/app/root/root.component.cy.ts
@@ -0,0 +1,7 @@
+import { RootComponent } from './root.component'
+
+describe('RootComponent', () => {
+  it('should mount', () => {
+    cy.mount(RootComponent)
+  })
+})
---
+++ src/app/root/root.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: false,
+  templateUrl: './root.component.html',
+  styleUrl: './root.component.scss',
+})
+export class RootComponent {
+
+}
