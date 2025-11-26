5d9db50ee903da6c8f452c2bdb7e71065e8515c8
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-11-26 14:10:27 +0900

    feat: ✨ Add legal zone
    Needs split component with data and ui

---
+++ src/app/legal-zone-table/legal-zone-table.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+interface Source {
+  key: string;
+  value: string;
+}
+
+@Component({
+  selector: 'app-legal-zone-table',
+  imports: [MatTableModule],
+  templateUrl: './legal-zone-table.html',
+  styleUrl: './legal-zone-table.scss',
+})
+export class LegalZoneTable {
+  public columns = ['key', 'value'];
+  public src: Source[] = [
+    {
+      key: '名前',
+      value: 'Hiroaki Yamamoto'
+    },
+    {
+      key: '所在地、電話番号、及びメールアドレス',
+      value: '請求書もしくは契約書にて記載'
+    },
+    {
+      key: '運営統括責任者',
+      value: 'Hiroaki Yamamoto'
+    },
+    {
+      key: '追加手数料等の追加料金',
+      value: 'エージェントによる仲介費用、銀行振込手数料、消費税、その他委託者起因による料金',
+    },
+    {
+      key: '交換および返品（返金ポリシー）',
+      value: `エージェントによる仲介の場合は、そのエージェントの返金ポリシーに準拠。
+      それ以外は契約書に別途記載`
+    },
+    {
+      key: '引渡(稼働開始)時期',
+      value: `契約書に記載`
+    },
+    {
+      key: '受け付け可能な決済手段',
+      value: '請求書もしくは契約書にて記載'
+    },
+    {
+      key: '決済期間',
+      value: `エージェントによる仲介の場合は、そのエージェントのポリシーに準拠。
+      それ以外は契約書に準ずる。`
+    },
+    {
+      key: '販売価格',
+      value: '商品の性質上、販売価格は契約時に決定。'
+    },
+  ];
+}
--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+
 import { LinkListComponent } from '../link-list/link-list.component';
+import { LegalZoneTable } from '../legal-zone-table/legal-zone-table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrl: './root.component.scss',
-  imports: [LinkListComponent]
+  imports: [MatTabsModule, LinkListComponent, LegalZoneTable]
 })
 export class RootComponent {
 
