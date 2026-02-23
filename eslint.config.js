8d86c37b09c3823f7f4a20cb08e6003f17b4ba37
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2026-02-23 16:09:33 +0900

    chore: refactor ESLint configuration to remove defineConfig and restructure for clarity

--- eslint.config.js
+++ eslint.config.js
@@ -1,53 +1,58 @@
-import { defineConfig } from 'eslint/config';
 import tslint from 'typescript-eslint';
 import ngLint from 'angular-eslint';
+import { parser } from 'typescript-eslint';
 
-export default defineConfig(
-  tslint.configs.recommended,
-  ngLint.configs.tsRecommended,
-  // ...ngLint.configs.templateRecommended,
-  // ...ngLint.configs.templateAccessibility,
-  {
-    ignores: [
-      ".angular/",
-      "coverage/",
-      "dist/",
-      "node_modules/",
-      "cypress/",
-      "cypress.config.ts",
-    ]
+const tsLintConfig = {
+  files: ['src/**/*.ts'],
+  languageOptions: {
+    parserOptions: {
+      project: ['tsconfig.*?.json'],
+      createDefaultProgram: true,
+    },
+    parser: tslint.parser,
   },
-  {
-    files: ['src/**/*.ts'],
-    languageOptions: {
-      parserOptions: {
-        project: ['tsconfig.*?.json'],
-        createDefaultProgram: true,
-      },
-    },
-    plugins: {
-      'tslint': tslint.plugin,
-      'ngLint': ngLint.tsPlugin,
-    },
-    processor: ngLint.processInlineTemplates,
-    rules: {
-      'ngLint/directive-selector': [
-        'error',
-        { type: 'attribute', style: 'camelCase' }
-      ],
-      'ngLint/component-selector': [
-        'error',
-        { type: 'element', prefix: 'app', style: 'kebab-case' },
-      ],
-      'ngLint/prefer-standalone': 0,
-      quotes: ['error', 'single', { allowTemplateLiterals: true }],
-      'tslint/no-misused-promises': ['error'],
-    },
+  plugins: {
+    'tslint': tslint.plugin,
+    'ngLint': ngLint.tsPlugin,
   },
-  // {
-  //   files: ['src/**/*.html'],
-  //   rules: {
-  //     'max-len': ['error', { code: 79 }],
-  //   },
-  // },
-);
+  processor: ngLint.processInlineTemplates,
+  rules: {
+    'ngLint/directive-selector': [
+      'error',
+      { type: 'attribute', style: 'camelCase' }
+    ],
+    'ngLint/component-selector': [
+      'error',
+      { type: 'element', prefix: 'app', style: 'kebab-case' },
+    ],
+    'ngLint/prefer-standalone': 0,
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'tslint/no-misused-promises': ['error'],
+  },
+};
+
+// const htmlConfig = {
+//   files: ['src/**/*.html'],
+//   plugins: ngLint.templatePlugin,
+//   languageOptions: {
+//     parser: ngLint.templateParser,
+//   },
+//   rules: {
+//     'max-len': ['error', { code: 79 }],
+//   },
+// };
+
+const exclude = {
+  ignores: [
+    ".angular/",
+    "coverage/",
+    "dist/",
+    "node_modules/",
+    "cypress/",
+    "cypress.config.ts",
+  ]
+};
+
+export default [
+  tsLintConfig, exclude,
+];
